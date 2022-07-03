b53ce5133d59ca4738b0cfbe1b91ee994ca588e5
Gilson Conceição <87782529+gilsonconceicao@users.noreply.github.com>
2022-07-03 18:34:50 -0300

    project with products

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const uuid = require('uuid'); 
+const bodyParser = require('body-parser');
+const port = 3000; 
+
+const app = express(); 
+
+app.get
+
+app.listen(port, () => {
+    console.log('Server Running!')
+})
